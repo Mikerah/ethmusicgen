0c53604a7e9ddaf348d622b75e0615e10132389e
Mikerah <mikerah14@gmail.com>
2018-01-01 19:41:24 -0500

    Completed an initial version of the MusicGenerator smart contract tested in Remix IDE with one warning

---
+++ contracts/MusicGenerator.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.19;
+
+/*
+ Contract for Music Generator
+*/
+
+contract MusicGenerator {
+    
+    // Owner state variable
+    address owner;
+    
+    // Constructor
+    function MusicGenerator() public {
+        owner = msg.sender;
+    }
+    
+    // Event of a song's purchase
+    event Purchase (address owner, uint price, uint priceToListen, bytes32 songHash);
+    
+    // Song struct
+    struct Song {
+        uint price;
+        uint priceToListen;
+        bytes32 songHash;
+    }
+    
+    // Mapping of owners to songs;
+    mapping (address => Song) public songOwnership;
+    
+    // function used to buy the generated song
+    function buySong(uint priceToListen, bytes32 hash) public payable {
+        songOwnership[owner].price = msg.value;
+        songOwnership[owner].priceToListen = priceToListen;
+        songOwnership[owner].songHash = hash;
+        
+        Purchase(owner, msg.value, priceToListen, hash);
+    }
+    
+    // function used to listen to a song 
+    function listenToSong(address listener) public payable {
+        if(listener.balance >= songOwnership[owner].priceToListen){
+            owner.transfer(songOwnership[owner].priceToListen);
+        }
+    }
+    
+}
